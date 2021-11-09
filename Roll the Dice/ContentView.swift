a061d45ed84eb33fa0906d07236da7819bf72154
Jack Louis <jbl5925@gmail.com>
2021-11-09 10:32:57 -0600

    Missed out on first day, created code for initial view, dice images, and rotational effects

--- Roll the Dice/ContentView.swift
+++ Roll the Dice/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var randomValue = 0
+    @State private var rotation = 0.0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Dice Roll")
+                .font(.title)
+                .padding()
+            Image("pips \(randomValue)")
+                .resizable()
+                .frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .rotationEffect(.degrees(rotation))
+                .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
+                .padding()
+                .onTapGesture {
+                    randomValue = Int.random(in: 1...6)
+                }
+            Spacer()
+        }
     }
 }
 
